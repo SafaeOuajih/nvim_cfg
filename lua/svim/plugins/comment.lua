e4c21674e5bc2c5c4808e6c862a8e98937fd36a2
Safae Ouajih <ouajih.safae@gmail.com>
2026-07-27 18:50:02 +0200

    comment: map cc to toggle comments
    Signed-off-by: Safae Ouajih <ouajih.safae@gmail.com>

--- lua/svim/plugins/comment.lua
+++ lua/svim/plugins/comment.lua
@@ -1,7 +1,15 @@
--- Comment.nvim: toggle comments with gc / gcc across languages.
 ---@type LazyPluginSpec
 return {
   'numToStr/Comment.nvim',
-  -- Skip empty lines when commenting a range.
-  opts = { ignore = '^$' },
+  opts = {
+    ignore = '^$', -- Skip empty lines when commenting a range
+    toggler = {
+      line = 'cc', -- replaces gcc
+      block = 'cbc', -- replaces gbc
+    },
+    opleader = {
+      line = 'cc', -- replaces gc (for motions/visual)
+      block = 'cb', -- replaces gb
+    },
+  },
 }
